45c2c11b3bcc0c40bb5db2153edc228a7639798a
captainsparrow10 <javier1009rm@gmail.com>
2022-11-19 11:27:55 -0500

    capitulo 5

--- components/Libro/Capitulos.jsx
+++ components/Libro/Capitulos.jsx
@@ -3,7 +3,7 @@ import Capitulo1 from './Capitulo1'
 import Capitulo2 from './Capitulo2'
 import Capitulo3 from './Capitulo3'
 import Capitulo4 from './Capitulo4'
-import Capitulo5 from './capitulo5'
+import Capitulo5 from './Capitulo5'
 import Capitulo6 from './Capitulo6'
 import Capitulo7 from './Capitulo7'
 export default function Capitulos() {
